8ce6763495f760c82e582b532ceddec7e3d63885
Caio Lorenzetti Martinelli <caio.martinelli@hotmail.com>
2021-05-08 20:01:45 -0300

    Add folder

---
+++ notebooks/transe_link_pred/train-full-l2-0.sh
@@ -0,0 +1,11 @@
+dglke_train \
+--dataset ogbn-arxiv \
+--data_path data/dataset_dglke \
+--data_files entities.tsv relations.tsv \
+ogbn-arxiv-edges-train.tsv \
+--format udd_hrt \
+--model_name TransE_l2 --hidden_dim 10 \
+--gamma 16.0 --lr 0.01 --batch_size 1000 \
+--neg_sample_size 200 --max_step 5000 \
+--log_interval 100 --regularization_coef 1.00E-07 \
+--gpu 0
---
+++ notebooks/transe_link_pred/trainl1-1.sh
@@ -0,0 +1,14 @@
+dglke_train \
+--dataset ogbn-arxiv \
+--data_path data/dataset_dglke \
+--data_files entities.tsv relations.tsv \
+ogbn-arxiv-edges-train.tsv ogbn-arxiv-edges-valid.tsv \
+ogbn-arxiv-edges-test.tsv \
+--format udd_hrt \
+--model_name TransE_l1 --hidden_dim 20 \
+--gamma 16.0 --lr 0.01 --batch_size 2000 --batch_size_eval 2000 \
+--neg_sample_size 200 --max_step 5000 \
+--log_interval 100 \
+--batch_size_eval 16 --regularization_coef 1.00E-07 \
+--gpu 0 \
+--test
