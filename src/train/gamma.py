92f3f6bc665b7a4983390853d59838401c8391f8
Caio Martinelli <caio.martinelli@hotmail.com>
2022-04-24 19:50:20 -0300

    Add train function to GammaGraphSage

--- src/train/gamma.py
+++ src/train/gamma.py
@@ -1,6 +1,8 @@
+import logging
+import os
 from sklearn.metrics import roc_auc_score
 import torch
-from torch_geometric.data import DataLoader
+from torch_geometric.loader import DataLoader
 from torch_geometric.utils import negative_sampling
 
 from src.models import GraphSAGE, LinkPredictor
@@ -8,28 +10,52 @@ from src.models import GraphSAGE, LinkPredictor
 
 HIDDEN_CHANNELS = 50
 DROPOUT = 0.5
-BATCH_SIZE = 128 * 1024
 LEARNING_RATIO = 0.005
-N_LAYERS = 1
-EPOCHS = 300
-RUNS = 10
+MODEL_PATH_PAT = 'models/gamma/graph_sage/{n_layers}layers_epoch{epoch:04d}.pt'
+PREDICTOR_PATH_PAT = 'models/gamma/link_predictor/{n_layers}layers_epoch{epoch:04d}.pt'
+METRICS_PATH = 'data/metrics/gamma_graph_sage_{n_layers}layers.pt'
+METRICS_COLS = [
+    'run',
+    'epoch',
+    'loss_train',
+    'loss_val',
+    'loss_test',
+    'auc_train',
+    'auc_val',
+    'auc_test',
+]
 
 
 class GammaGraphSage():
 
-    def __init__(self, device, num_nodes, eval_steps=5):
+    def __init__(
+            self,
+            device,
+            num_nodes,
+            eval_steps=5,
+            n_layers=1,
+            epochs=300,
+            batch_size=128 * 1024,
+            run=0):
+        self.n_layers = n_layers
         self.initialize_models_data(device, num_nodes)
         self.eval_steps = eval_steps
+        self.epochs = epochs
+        self.batch_size = batch_size
+        self.model_path_pat = MODEL_PATH_PAT
+        self.predictor_path_pat = PREDICTOR_PATH_PAT
+        self.model_metrics_path = METRICS_PATH
+        self.run = run
 
     def initialize_models_data(self, device, num_nodes):
 
-        self.model = GraphSAGE(N_LAYERS,
+        self.model = GraphSAGE(self.n_layers,
                                HIDDEN_CHANNELS,
                                HIDDEN_CHANNELS,
                                DROPOUT).to(device)
-
+        self.model.reset_parameters()
         self.predictor = LinkPredictor().to(device)
-
+        self.predictor.reset_parameters()
         self.embedding = torch.nn.Embedding(
             num_nodes,
             HIDDEN_CHANNELS).to(device)
@@ -86,14 +112,14 @@ class GammaGraphSage():
 
         return total_loss / total_examples
 
-    def predict(self, edges, adj_t):
+    def forward(self, edges, adj_t):
         self.model.eval()
         self.predictor.eval()
         emb_weight = self.embedding.weight
-        
+
         h = self.model(emb_weight, adj_t)
         probas = self.predictor(h[edges[0]], h[edges[1]])
-        
+
         return probas
 
     def is_same_class(self, edges, y):
@@ -109,12 +135,12 @@ class GammaGraphSage():
             adj_t,
             y):
 
-        proba_pos_train = self.predict(edges_train, adj_t)
-        proba_pos_val = self.predict(edges_val, adj_t)
-        proba_pos_test = self.predict(edges_test, adj_t)
+        proba_pos_train = self.forward(edges_train, adj_t)
+        proba_pos_val = self.forward(edges_val, adj_t)
+        proba_pos_test = self.forward(edges_test, adj_t)
 
-        proba_neg_val = self.predict(neg_edges_val, adj_t)
-        proba_neg_test = self.predict(neg_edges_test, adj_t)
+        proba_neg_val = self.forward(neg_edges_val, adj_t)
+        proba_neg_test = self.forward(neg_edges_test, adj_t)
 
         # Loss evaluation
 
@@ -160,3 +186,100 @@ class GammaGraphSage():
         auc_test = roc_auc_score(theta_test, proba_pos_test)
 
         return loss_val, loss_test, auc_train, auc_val, auc_test
+
+    def save_models(self, epoch):
+        model_path = self.model_path_pat.format(
+            n_layers=self.n_layers,
+            epoch=epoch)
+        model_folder = model_path.rsplit('/', 1)[0]
+        if not os.path.exists(model_folder):
+            os.makedirs(model_folder)
+        predictor_path = self.predictor_path_pat.format(
+            n_layers=self.n_layers,
+            epoch=epoch)
+        predictor_folder = predictor_path.rsplit('/', 1)[0]
+        if not os.path.exists(predictor_folder):
+            os.makedirs(predictor_folder)
+        torch.save(self.model, model_path)
+        torch.save(self.predictor, predictor_path)
+
+    def save_metrics(
+            self,
+            epoch,
+            loss_train,
+            loss_val,
+            loss_test,
+            auc_train,
+            auc_val,
+            auc_test):
+
+        metrics_path = self.model_metrics_path.format(n_layers=self.n_layers)
+        metrics_folder = metrics_path.rsplit('/', 1)[0]
+        if not os.path.exists(metrics_folder):
+            os.makedirs(metrics_folder)
+        if not os.path.exists(metrics_path):
+            header = ','.join(METRICS_COLS) + '\n'
+            with open(metrics_path, 'w') as stream:
+                stream.write(header)
+
+        metrics = [
+            self.run,
+            epoch,
+            loss_train,
+            loss_val,
+            loss_test,
+            auc_train,
+            auc_val,
+            auc_test
+        ]
+
+        metrics_row = ','.join(str(x) for x in metrics) + '\n'
+        with open(metrics_path, 'a') as stream:
+            stream.write(metrics_row)
+
+    def train(
+            self,
+            edges_train,
+            edges_val,
+            edges_test,
+            neg_edges_val,
+            neg_edges_test,
+            adj_t,
+            y):
+
+        for epoch in range(1, 1 + self.epochs):
+            loss_train = self.train_epoch(
+                edges_train,
+                self.batch_size,
+                adj_t)
+
+            if epoch % self.eval_steps == 0:
+                loss_val, loss_test, auc_train, auc_val, auc_test = self.eval(
+                    edges_train,
+                    edges_val,
+                    edges_test,
+                    neg_edges_val,
+                    neg_edges_test,
+                    adj_t,
+                    y)
+
+                logging.info(
+                    f'Run: {self.run:04d}, '
+                    f'Epoch: {epoch:04d}, '
+                    f'Train Loss: {loss_train:.4f}, '
+                    f'Valid loss: {loss_val:.4f}, '
+                    f'Test loss: {loss_test:.4f}, '
+                    f'Train AUC: {auc_train:.4f}, '
+                    f'Valid AUC: {auc_val:.4f}, '
+                    f'Test AUC: {auc_test:.4f}')
+
+                self.save_models(epoch)
+
+                self.save_metrics(
+                    epoch,
+                    loss_train,
+                    loss_val.item(),
+                    loss_test.item(),
+                    auc_train,
+                    auc_val,
+                    auc_test)
