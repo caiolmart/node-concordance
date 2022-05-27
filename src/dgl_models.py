6e0ce3a076b491711c80a945bd0f0298cbabb9f3
Caio Martinelli <caio.martinelli@hotmail.com>
2022-05-26 23:56:23 -0300

    Add EdgeWeigher model to dgl_models module

--- src/dgl_models.py
+++ src/dgl_models.py
@@ -43,3 +43,24 @@ class GraphSAGE(torch.nn.Module):
             x = F.dropout(x, p=self.dropout, training=self.training)
         x = self.convs[-1](graph, x, edge_weight=edge_weight)
         return x.log_softmax(dim=-1)
+
+
+class EdgeWeigher(torch.nn.Module):
+    def __init__(
+            self,
+            base_weights,
+            bias=True):
+        super(EdgeWeigher, self).__init__()
+
+        self.base_weights = base_weights
+        self.lin = torch.nn.Linear(1, 1, bias=bias)
+
+    def reset_parameters(self):
+        self.lin.weight.data.fill_(1)
+        self.lin.bias.data.fill_(0)
+
+    def forward(self):
+        edge_weights = self.lin(self.base_weights)
+        edge_weights = torch.sigmoid(edge_weights)
+
+        return edge_weights
