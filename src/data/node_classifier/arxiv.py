a124922458e3afaea9034c5fb6e42d032228531d
Caio Martinelli <caio.martinelli@hotmail.com>
2022-06-19 12:18:26 -0300

    Add PyG functions to data loaders

--- src/data/node_classifier/arxiv.py
+++ src/data/node_classifier/arxiv.py
@@ -1,13 +1,17 @@
 import pandas as pd
 import torch
-from ogb.nodeproppred import DglNodePropPredDataset
+import torch_geometric.transforms as T
+from torch_geometric.data.data import Data
+from ogb.nodeproppred import DglNodePropPredDataset, PygNodePropPredDataset
 from dgl.heterograph import DGLHeteroGraph
 
 
 EDGES_SEED = 12345
 TEST_VAL_PROPORTION = 0.025
-DATA_FOLDER = 'data/node_classifier/'
-DATA_NAME = 'ogbn-arxiv'
+DATA_FOLDER_DGL = 'data/node_classifier/'
+DATA_NAME_DGL = 'ogbn-arxiv'
+DATA_FOLDER_PYG = 'data/gamma/'
+DATA_NAME_PYG = 'ogbn-arxiv'
 
 
 def remove_rows(tensor, indices):
@@ -16,13 +20,26 @@ def remove_rows(tensor, indices):
     return tensor[:, mask]
 
 
-def load_dataset():
-    dataset = DglNodePropPredDataset(name=DATA_NAME, 
-                                     root=DATA_FOLDER)
+def load_dataset_dgl():
+    dataset = DglNodePropPredDataset(name=DATA_NAME_DGL, 
+                                     root=DATA_FOLDER_DGL)
     return dataset
 
 
-def get_symmetric_graph(dataset) -> DGLHeteroGraph:
+def get_symmetric_graph_dgl(dataset) -> DGLHeteroGraph:
     graph = dataset[0][0]
     graph.add_edges(graph.edges()[1], graph.edges()[0])
     return graph
+
+
+def load_dataset_pyg():
+    dataset = PygNodePropPredDataset(name=DATA_NAME_PYG, 
+                                     root=DATA_FOLDER_PYG)
+    return dataset
+
+
+def data_to_sparse_symmetric_pyg(data: Data) -> Data:
+    T.ToSparseTensor()(data)
+    data.adj_t = data.adj_t.to_symmetric()
+
+    return data
