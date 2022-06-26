e840040eaf430c5084c5927077d30fb31f57c62f
Caio Martinelli <caio.martinelli@hotmail.com>
2022-06-26 19:46:20 -0300

    Add get_edge_weights_pyg function to data.node_classifier.arxiv module

--- src/data/node_classifier/arxiv.py
+++ src/data/node_classifier/arxiv.py
@@ -4,6 +4,7 @@ import torch_geometric.transforms as T
 from torch_geometric.data.data import Data
 from ogb.nodeproppred import DglNodePropPredDataset, PygNodePropPredDataset
 from dgl.heterograph import DGLHeteroGraph
+from torch_sparse import SparseTensor
 
 
 EDGES_SEED = 12345
@@ -43,3 +44,21 @@ def data_to_sparse_symmetric_pyg(data: Data) -> Data:
     data.adj_t = data.adj_t.to_symmetric()
 
     return data
+
+
+def get_edge_weights_pyg(
+        csv_path: str,
+        n_nodes: int,
+        device: torch.device,
+        source_col='source',
+        target_col='target',
+        weight_col='gamma_pred'
+    ) -> SparseTensor:
+    edges_gamma_df = pd.read_csv(csv_path)
+    edge_weights = SparseTensor.from_torch_sparse_coo_tensor(
+        torch.sparse_coo_tensor(
+            edges_gamma_df[[source_col, target_col]].values.T,
+            edges_gamma_df[weight_col].values,
+            size=(n_nodes, n_nodes)))\
+        .to(device)
+    return edge_weights
