39342d07e5fd7de0878c2c2803a8b73b9d4c28fb
Caio Martinelli <caio.martinelli@hotmail.com>
2022-11-26 17:04:05 -0300

    Separate Training Edges and AUC edges

--- src/data/gamma/positional_arxiv.py
+++ src/data/gamma/positional_arxiv.py
@@ -1,14 +1,14 @@
+from typing import Tuple
 import torch
-from ogb.nodeproppred import PygNodePropPredDataset
 import torch_geometric.transforms as T
+from ogb.nodeproppred import PygNodePropPredDataset
 from torch_geometric.data.data import Data
 from torch_geometric.utils import negative_sampling
 
-
 EDGES_SEED = 12345
-TEST_VAL_PROPORTION = 0.025
-DATA_FOLDER = 'data/gamma/'
-DATA_NAME = 'ogbn-arxiv'
+TEST_VAL_PROPORTION = 0.01
+DATA_FOLDER = "data/gamma/"
+DATA_NAME = "ogbn-arxiv"
 
 
 def remove_rows(tensor, indices):
@@ -17,21 +17,63 @@ def remove_rows(tensor, indices):
     return tensor[:, mask]
 
 
-def load_data() -> Data:
-    dataset = PygNodePropPredDataset(name=DATA_NAME, 
-                                     root=DATA_FOLDER)
+def load_dataset() -> Data:
+    dataset = PygNodePropPredDataset(name=DATA_NAME, root=DATA_FOLDER)
+    return dataset
+
+def get_train_val_test_edges_auc(
+        dataset: PygNodePropPredDataset,
+        remove_from_data=False,
+        device=None
+    ) -> Tuple[Data, torch.tensor, torch.tensor, torch.tensor]:
+
+    # Creating node -> flag map (train, valid, test)
+    idx_split = dataset.get_idx_split()
+    idx_split_map = {}
+    for idx in idx_split['train'].numpy():
+        idx_split_map[idx] = 0
+    for idx in idx_split['valid'].numpy():
+        idx_split_map[idx] = 1
+    for idx in idx_split['test'].numpy():
+        idx_split_map[idx] = 2
+
     data = dataset[0]
-    return data
+
+    # Creating edge flags
+    node_flags = data.edge_index.clone()
+    node_flags.apply_(lambda val: idx_split_map.get(val))
+    edge_flags = torch.max(node_flags, axis=0).values
+
+    # Getting train, validation and test edges
+    train_mask = edge_flags == 0
+    edges_train_auc = data.edge_index[:, train_mask]
+
+    val_mask = edge_flags == 1
+    edges_val_auc = data.edge_index[:, val_mask]
+
+    test_mask = edge_flags == 2
+    edges_test_auc = data.edge_index[:, test_mask]
+
+    # Removing validation and test edges
+    if remove_from_data:
+        data.edge_index = data.edge_index[:, train_mask]
+
+    if device:
+        data = data.to(device)
+        edges_train_auc = edges_train_auc.to(device)
+        edges_val_auc = edges_val_auc.to(device)
+        edges_test_auc = edges_test_auc.to(device)
+
+    return data, edges_train_auc, edges_val_auc, edges_test_auc
 
 
-def get_val_test_edges(data: Data, remove_from_data=True, device=None):
+def get_val_test_edges_link_pred(data: Data, remove_from_data=True, device=None):
     # Getting test and validation edges
     torch.manual_seed(EDGES_SEED)
     idx = torch.randperm(data.edge_index.size(1))
-    n_val_edges = n_test_edges =\
-        int(data.edge_index.size(1) * TEST_VAL_PROPORTION)
+    n_val_edges = n_test_edges = int(data.edge_index.size(1) * TEST_VAL_PROPORTION)
     val_idx = idx[:n_val_edges]
-    test_idx = idx[n_val_edges:(n_val_edges + n_test_edges)]
+    test_idx = idx[n_val_edges : (n_val_edges + n_test_edges)]
     edges_val = data.edge_index[:, val_idx]
     edges_test = data.edge_index[:, test_idx]
 
@@ -39,12 +81,14 @@ def get_val_test_edges(data: Data, remove_from_data=True, device=None):
         data.edge_index,
         num_nodes=data.x.size(0),
         num_neg_samples=edges_val.size(1),
-        method='sparse')
+        method="sparse",
+    )
     neg_edges_test = negative_sampling(
         data.edge_index,
         num_nodes=data.x.size(0),
         num_neg_samples=edges_test.size(1),
-        method='sparse')
+        method="sparse",
+    )
 
     # Removing validation and test edges
     if remove_from_data:
